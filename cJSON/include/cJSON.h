4c26da30d5b2fbf07ad55f9dca488d09873c03e8
Victoria Peshkova <Victoria.Peshkova@elecard.ru>
2013-09-12 15:14:36 +0700

    [+] App: Fixed compilation errors in cJSON.*

--- cJSON/include/cJSON.h
+++ cJSON/include/cJSON.h
@@ -54,7 +54,7 @@ typedef struct cJSON {
 } cJSON;
 
 typedef struct cJSON_Hooks {
-      void *(*malloc_fn)(size_t sz);
+      void *(*malloc_fn)(unsigned int sz);
       void (*free_fn)(void *ptr);
 } cJSON_Hooks;
 
--- cJSON/src/cJSON.c
+++ cJSON/src/cJSON.c
@@ -43,12 +43,12 @@ static int cJSON_strcasecmp(const char *s1,const char *s2)
 	return tolower(*(const unsigned char *)s1) - tolower(*(const unsigned char *)s2);
 }
 
-static void *(*cJSON_malloc)(size_t sz) = malloc;
+static void *(*cJSON_malloc)(unsigned int sz) = malloc;
 static void (*cJSON_free)(void *ptr) = free;
 
 static char* cJSON_strdup(const char* str)
 {
-      size_t len;
+      unsigned int len;
       char* copy;
 
       len = strlen(str) + 1;
