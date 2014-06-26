515bc3d85bd3a0711feea3ca156d2315c91ba1f4
Victoria Peshkova <Victoria.Peshkova@elecard.ru>
2014-06-26 15:37:42 +0700

    [-] Fusion: Fixed memleak

--- StbMainApp/src/interface.c
+++ StbMainApp/src/interface.c
@@ -2195,6 +2195,7 @@ void interface_displayCreepline(void)
 				0, 1);
 				*/
 				draw_string(copyCreep, interfaceInfo.screenWidth - position, interfaceInfo.screenHeight - 40);
+				free(copyCreep);
 		}
 		interface_flipStatusbarSurface();
 	}
